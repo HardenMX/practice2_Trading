d32b917edde2a3c4a868d6053be601ccdf8759f7
Fmorejon <morejonff@gmail.com>
2024-03-20 12:28:30 -0600

    cleanup

---
+++ trading.js
@@ -0,0 +1,14 @@
+const myFood = document.getElementById('myFood');
+const myWood = document.getElementById('myWood');
+const myGold = document.getElementById('myGold');
+const myStone = document.getElementById('myStone');
+
+const myFoodUI = document.getElementById('myFoodUI');
+const myWoodUI = document.getElementById('myWoodUI');
+const myGoldUI = document.getElementById('myGoldUI');
+const myStoneUI = document.getElementById('myStoneUI');
+
+// myFoodUI.innerHTML = "500";
+// myWoodUI.innerHTML = "600";
+// myGoldUI.innerHTML = "700";
+// myStoneUI.innerHTML = "800";
