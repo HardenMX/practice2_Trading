e18387a5d505d922aff9603f9d3600a7d8d6cf6a
Fmorejon <morejonff@gmail.com>
2024-03-21 09:55:38 -0600

    workingUIup

--- trading.js
+++ trading.js
@@ -1,8 +1,16 @@
+// picking up HTML elements
+const foodInput = document.getElementById('food');
+const woodInput = document.getElementById('wood');
+const goldInput = document.getElementById('gold');
+const stoneInput = document.getElementById('stone');
+
+const submitBtn = document.getElementById('submitBtn');
+// resource inputs
 const myFood = document.getElementById('myFood');
 const myWood = document.getElementById('myWood');
 const myGold = document.getElementById('myGold');
 const myStone = document.getElementById('myStone');
-
+// UI showing actual resources
 const myFoodUI = document.getElementById('myFoodUI');
 const myWoodUI = document.getElementById('myWoodUI');
 const myGoldUI = document.getElementById('myGoldUI');
@@ -13,6 +21,10 @@ myWoodUI.innerHTML = "0";
 myGoldUI.innerHTML = "0";
 myStoneUI.innerHTML = "0";
 
-function updateUI () {
-    
+
+// SubmitBtn
+submitBtn.addEventListener('click', submitResources());
+
+function submitResources () {
+    myFoodUI.innerHTML = foodInput.textContent;
 }
